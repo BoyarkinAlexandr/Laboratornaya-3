70152818109e48dc2ebae8057ffafac67a31c0c6
alexandrboyarkin <generator185@gmail.com>
2021-12-26 14:20:46 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect
+import psycopg2
+
+app=Flask(__name__)
+
+conn = psycopg2.connect(database="service",
+                        user="alexandrboyarkin",
+                        password="1234",
+                        host="localhost",
+                        port="5432")
+cursor = conn.cursor()
+
+@app.route('/login/', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        if request.form.get("login"):
+            username = request.form.get('username')  # запрос к данным формы
+            password = request.form.get('password')
+            cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+            records = list(cursor.fetchall())
+            if len(records) == 0:
+                return render_template('error.html')
+            else:
+                return render_template('account.html', full_name=records[0][1])
+        elif request.form.get("registration"):
+            return redirect("/registration/")
+
+    return render_template('login.html')
